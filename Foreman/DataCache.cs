723aade96e5a05af56fc3ecbe2e7f3c51601ed9b
Kamikaetzchen <31161185+Kamikaetzchen@users.noreply.github.com>
2020-11-08 12:14:00 +0100

    revert or something

--- Foreman/DataCache.cs
+++ Foreman/DataCache.cs
@@ -385,7 +385,7 @@ namespace Foreman
                 patch = reader.ReadUInt16();
                 dev = reader.ReadUInt16();
 
-                if (major > 0 || (major == 0 && minor >= 17))
+                if (minor >= 17 )
                 {
                     var noop = reader.ReadByte();
                 }
